2de4ff765c7f04a54c3d72ba2a122ccc7b552108
prabingeorge <prabingeorge@gmail.com>
2026-03-03 12:13:58 +0530

    Added the API end point URL

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = 'https://food-6qql.onrender.com'; // 'http://localhost:4000';
     const [token, setToken] = useState(localStorage.getItem("token") ? localStorage.getItem("token") : "");
     const [food_list, setFoodList] = useState([]);
 
