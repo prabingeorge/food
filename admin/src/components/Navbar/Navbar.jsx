47245051f9633677385f968bd53321f421848ad0
prabingeorge <prabingeorge@gmail.com>
2026-03-03 15:51:15 +0530

    Added code for logo path

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Navbar.css';
 import { FaUserLarge } from "react-icons/fa6";
+import logoHeader from "../../assets/icons/logo-header.png";
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img className='logo' src={`../../assets/icons/logo-header.png`} alt="logo" />
+        <img className='logo' src={logoHeader} alt="logo" />
         <FaUserLarge />
         {/* <img className='profile' src="profile" alt="profile" /> */}
     </div>
