696a9958ab3ab1c24cc99111481cacf963be1b9a
prabingeorge <prabingeorge@gmail.com>
2026-03-03 14:14:11 +0530

    Added the menu images in the public folder

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -14,7 +14,7 @@ const ExploreMenu = ({ category, setCategory }) => {
             <div className="explore-menu-list">
                 {menu_list?.map((item, index) => {
                     return (<div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item?.menu_name)} key={index} className='explore-menu-list-item'>
-                        <img className={category === item?.menu_name ? "active" : ""} src={"src/assets/" + item?.menu_image} alt="" />
+                        <img className={category === item?.menu_name ? "active" : ""} src={"../../../public/images/menu/" + item?.menu_image} alt="" />
                         <p>{item.menu_name}</p>
                     </div>)
                 })}
