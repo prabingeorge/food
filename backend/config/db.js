90653d3c3ce0e093c7ccd1c08d5d5fd5ee8d0343
prabingeorge <prabingeorge@gmail.com>
2026-03-05 16:22:42 +0530

    Added .env file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://prabingeorge:s6XnhzRlEEGqVofa@cluster0.7a5ls.mongodb.net/food-del2').then(()=>console.log("DB Connected."))
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB Connected."))
+    // await mongoose.connect('mongodb+srv://prabingeorge:s6XnhzRlEEGqVofa@cluster0.7a5ls.mongodb.net/food-del2').then(()=>console.log("DB Connected."))
 };
