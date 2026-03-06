2417dcd0ed444a049fbf18ea989ad1eafbb09301
prabingeorge <prabingeorge@gmail.com>
2026-03-06 19:49:48 +0530

    Added code for frontend url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = 'https://amo-ra-food.onrender.com'; // 'http://localhost:5174';
+    const frontendUrl = process.env.FRONTEND_URL;
 
     try {
         const newOrder = new orderModel({
@@ -45,8 +45,8 @@ const placeOrder = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: line_items,
             mode: 'payment',
-            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+            success_url: `${frontendUrl}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontendUrl}/verify?success=false&orderId=${newOrder._id}`,
         });
         res.json({ success: true, session_url: session.url });
     } catch (error) {
